b75be0f0b8500f9b220fba4a9cdeac63584afc96
Matthew Warman <warmanmr@gmail.com>
2023-03-25 06:46:49 -0400

    helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // create the Nest app
   const app = await NestFactory.create(AppModule);
+
+  // apply third-party middleware
+  app.use(helmet());
+
+  // start the application
   await app.listen(3000);
 }
 bootstrap();
