58889d98cccb8838462b2778e8ee93212a94b69e
Matthew Warman <warmanmr@gmail.com>
2023-03-26 05:41:53 -0400

    todos module

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { TodosService } from './todos.service';
+import { Todo } from './interfaces/todo.interface';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+
+  @Get()
+  findAll(): Todo[] {
+    return this.todosService.findAll();
+  }
+
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto): Todo {
+    return this.todosService.create(createTodoDto);
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Todo | null {
+    const todo = this.todosService.findById(id);
+    if (todo) {
+      return todo;
+    } else {
+      throw new NotFoundException();
+    }
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto): Todo {
+    const todo = this.todosService.update(id, updateTodoDto);
+    if (todo) {
+      return todo;
+    } else {
+      throw new NotFoundException();
+    }
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id: string): void {
+    this.todosService.delete(id);
+  }
+}
